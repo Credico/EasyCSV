8f55d110a22b869f6f3b8ebad7f0f658b633b903
Mathias Verraes <mathias@verraes.net>
2012-01-04 16:10:38 +0100

    trim values

--- lib/EasyCSV/Reader.php
+++ lib/EasyCSV/Reader.php
@@ -4,41 +4,42 @@ namespace EasyCSV;
 
 class Reader extends AbstractBase
 {
-    private $_headers;
-    private $_line;
+	private $_headers;
+	private $_line;
 
-    public function __construct($path, $delimiter = ',', $mode = 'r+')
-    {
-        parent::__construct($path, $delimiter, $mode);
-        $this->_line    = 0;
-    }
+	public function __construct($path, $delimiter = ',', $mode = 'r+')
+	{
+		parent::__construct($path, $delimiter, $mode);
+		$this->_line    = 0;
+	}
 
-    public function getRow()
-    {
-        if (($row = fgetcsv($this->_handle, 4096, $this->_delimiter, $this->_enclosure)) !== false) {
-            $this->_line++;
-            if (empty($this->_headers)) {
-              $this->_headers = $row;
-              return $this->getRow();
-            } else {
-              return array_combine($this->_headers, $row);
-            }
-        } else {
-            return false;
-        }
-    }
+	public function getRow()
+	{
+		if (($row = fgetcsv($this->_handle, 4096, $this->_delimiter, $this->_enclosure)) !== false) {
+			$row = array_map('trim', $row);
+			$this->_line++;
+			if (empty($this->_headers)) {
+				$this->_headers = $row;
+				return $this->getRow();
+			} else {
+				return array_combine($this->_headers, $row);
+			}
+		} else {
+			return false;
+		}
+	}
 
-    public function getAll()
-    {
-        $data = array();
-        while ($row = $this->getRow()) {
-            $data[] = $row;
-        }
-        return $data;
-    }
+	public function getAll()
+	{
+		$data = array();
+		while ($row = $this->getRow()) {
+			$data[] = $row;
+		}
+		return $data;
+	}
 
-    public function getLineNumber()
-    {
-        return $this->_line;
-    }
+	public function getLineNumber()
+	{
+		return $this->_line;
+	}
 }
